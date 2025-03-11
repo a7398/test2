238c2a304396dec3d40a09b5f6c08ed54f2a4688
Anna <denisova.2010@yandex.ru>
2025-03-11 22:43:24 +0300

    Delete 1file.py

--- 1file.py
+++
@@ -1,8 +0,0 @@
-import time
-import thread
-
-def get_thread(thread_name):
-	time.sleep(1)
-	print(name)
-threads = [threading.Thread(target=get_thread, args=(f"Thread-{i}",)) for i in range(1, 6)]
-for t in threads: t.start()	
